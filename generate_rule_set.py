0b708b6997047d256e35e8058a76dd27b6d8c3d4
Thảo mai tắm <109426941+Thaomtam@users.noreply.github.com>
2024-03-05 21:50:31 +0700

    Update generate_rule_set.py

--- generate_rule_set.py
+++ generate_rule_set.py
@@ -4,20 +4,17 @@ import json
 import re
 
 output_dir = "./rule-set"
-json_output_file = os.path.join(output_dir, "all_domains.json")
+json_output_file = "rule-set.json"
 
 def write_json_file(data, filepath):
     with open(filepath, "w") as f:
         json.dump(data, f, indent=4)
 
 def fetch_and_convert_url(url, convert_function, function_name):
-    filename = function_name + ".json"
-    filepath = os.path.join(output_dir, filename)
     response = requests.get(url)
     if response.ok:
         converted_data = convert_function(response.text)
-        write_json_file(converted_data, filepath)
-        return filepath
+        return converted_data
     return None
 
 def merge_domains(*args):
@@ -31,29 +28,13 @@ def extract_domains(data):
     domain_list = [re.findall(r"[\w\.-]+", line)[1] for line in data.splitlines() if line.strip() and not line.startswith("#")]
     return domain_list
 
-def extract_threat(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return extract_domains(response.text)
-    else:
-        print("Failed to fetch threat file.")
-        return []
-
-def extract_casino(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return extract_domains(response.text)
-    else:
-        print("Failed to fetch casino file.")
-        return []
-
-def extract_adservers(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return extract_domains(response.text)
-    else:
-        print("Failed to fetch adservers file.")
-        return []
+def extract_sources_and_merge(urls):
+    all_domains = []
+    for url in urls:
+        domain_list = fetch_and_convert_url(url, extract_domains, "")
+        if domain_list:
+            all_domains.extend(domain_list)
+    return all_domains
 
 def main():
     os.makedirs(output_dir, exist_ok=True)
@@ -61,13 +42,20 @@ def main():
     threat_url = "https://raw.githubusercontent.com/bigdargon/hostsVN/master/extensions/threat/filter.txt"
     casino_url = "https://raw.githubusercontent.com/bigdargon/hostsVN/master/extensions/gambling/filter.txt"
     adservers_url = "https://raw.githubusercontent.com/bigdargon/hostsVN/master/filters/adservers-all.txt"
+    adaway_url = "https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt"
+    MVPS_url = "https://winhelp2002.mvps.org/hosts.txt"
+    easylist_url = "https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_adservers.txt"
+    yoyo_url = "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext&useip=0.0.0.0"
+    anudeep_url = "https://hosts.anudeep.me/mirror/adservers.txt"
+    xiaomi_url = "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Xiaomi-Extension.txt"
+    dan_url = "https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt"
+    Kninja_url = "https://raw.githubusercontent.com/pantsufan/BlockAds/main/hosts"
+    Redirect_url = "https://blocklistproject.github.io/Lists/redirect.txt"
+    Facebook_url = "https://blocklistproject.github.io/Lists/facebook.txt"
 
-    threat_domains = extract_threat(threat_url)
-    casino_domains = extract_casino(casino_url)
-    adservers_domains = extract_adservers(adservers_url)
-
-    all_domains = merge_domains(threat_domains, casino_domains, adservers_domains)
-
+    urls = [threat_url, casino_url, adservers_url, adaway_url, MVPS_url, easylist_url, yoyo_url, anudeep_url, xiaomi_url, dan_url, Kninja_url, Redirect_url, Facebook_url]
+    all_domains = extract_sources_and_merge(urls)
+    
     write_json_file({"version": 1, "rules": [{"domain": all_domains}]}, json_output_file)
     os.system(f"sing-box rule-set compile --output rule-set.srs {json_output_file}")
 
